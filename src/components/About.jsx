8e93e60d41a50ed59c0d90d01494cc54f1866eee
Gabriel Torres <gabrieltorresp2009@gmail.com>
2024-04-05 17:51:02 -0300

    Change the About Component to the flex box

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function About(props){
-    return (
-        <div id="perfil" className="pt-28 h-screen w-full bg-white">
-            <h1 className="text-center text-[#33261D] text-xl lg:text-2xl">Meu nome é <span className="text-[#1FA1D8]">{props.name}</span>.</h1>
-            <div className="lg:flex lg:h-[24rem] lg:items-center lg:mt-4 ">
-            <div className="h-60  my-8 mx-auto lg:w-[25rem] lg:h-[25rem]">
-            
-                <div className="bg-[#1488FA] w-32 h-32 rounded-full relative left-[50%] lg:h-56 lg:w-56 lg:left-54 top-6 "></div>
-                <div className="bg-[#1488FA] w-8 h-8 rounded-full relative left-[24%] bottom-[50%]   sm:invisible md:invisible lg:visible  lg:w-12 lg:h-12 lg:left-6 lg:bottom-44 "></div>
-                <div className="bg-[#1488FA] w-8 h-8 rounded-full relative left-[66%] -bottom-[8%]  sm:invisible md:invisible lg:visible lg:w-12 lg:h-12 lg:left-80 lg:-bottom-10"></div>
-                <img className="w-44 h-44 rounded-full relative m-auto bottom-[70%] lg:w-80 lg:h-80 lg:bottom-[70%]" src={props.img} alt={props.alt} />
-                
-            </div>
-            
-                <div className="w-64 mx-auto  lg:w-[25rem]">
-                <p className="text-[#33261D] text-xl lg:text-2xl my-2">{props.fContent}</p>
-                <p className="text-[#33261D] text-xl lg:text-2xl my-2">{props.sContent}</p>
-                </div>
-            </div>
-            
+export default function About(props) {
+  const [warning, setWarning] = useState(false);
+
+  // Função para alternar o estado da mensagem de aviso
+  const toggleWarning = () => {
+    setWarning(!warning);
+  };
+
+  return (
+    <div
+      id="perfil"
+      className="flex flex-col justify-center gap-y-12 items-center h-screen w-full bg-white"
+    >
+      <h1 className="text-center text-[#33261D] text-xl md:text-2xl">
+        Meu nome é <span className="text-[#1FA1D8]">{props.name}</span>.
+      </h1>
+      <div className="flex text-center md:text-left flex-col gap-y-4 md:flex-row md:w-2/5 md:gap-x-12 justify-between items-center">
+        <img
+          className="w-1/2 rounded-full "
+          src={props.img}
+          alt={props.alt}
+        />
+        <div className="w-1/2">
+          <p className="text-[#33261D] text-xl md:text-2xl my-2">
+            {props.fContent}
+          </p>
+          <p className="text-[#33261D] text-xl md:text-2xl my-2">
+            {props.sContent}
+          </p>
         </div>
-    )
+      </div>
+      
+      {warning && (
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-4 rounded-lg shadow-lg">
+          <p className="text-gray-800">
+            Esse portfolio é bem antigo. Estou montando um novo para trazer meus
+            projetos mais atuais. Obrigado! ❤
+          </p>
+          <button
+            onClick={toggleWarning}
+            className="mt-2 px-4 py-2 bg-[#1FA1D8] text-white rounded-lg"
+          >
+            Fechar
+          </button>
+        </div>
+      )}
+
+      {/* Botão para exibir a mensagem de aviso */}
+      <button
+        onClick={toggleWarning}
+        className="px-4 py-2 bg-[#1FA1D8] uppercase text-lg text-white border-2 rounded-sm border-[#1FA1D8] hover:text-[#1FA1D8] hover:bg-transparent hover:scale-110"
+      >
+        Observação
+      </button>
+    </div>
+  );
 }
-
-
-
